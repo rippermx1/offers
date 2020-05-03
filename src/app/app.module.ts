8c749545268ab95b5c230cb13c4aa81553a42bc5
carlos valdés <valdescarlos17@gmail.com>
2020-05-02 21:12:09 -0400

    card container for offer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OfferComponent } from './components/offer/offer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OfferComponent
   ],
   imports: [
     BrowserModule,
