f1ce653d74df860504a71844e23a79b41e66ba30
carlos valdés <valdescarlos17@gmail.com>
2020-05-03 19:11:40 -0400

    offer list component updated ui

--- src/app/components/offer-list/offer-list.component.ts
+++ src/app/components/offer-list/offer-list.component.ts
@@ -15,14 +15,49 @@ export class OfferListComponent implements OnInit {
     this.offer_list.push(
       {
         id: 1,
-        image: 'http://lorempixel.com/400/100/food/food/',
-        description: 'Alter triticums ducunt ad elevatus. A falsis, quadra teres parma.'
+        image: 'http://lorempixel.com/400/100/food/',
+        discount: 15,
+        description: 'Alter triticums ducunt ad elevatus. A falsis, quadra teres parma.',
+        storeName: 'Tottus',
+        storeUrl: 'https://www.tottus.cl/tottus/',
+        storeImage: 'tottus.jpg'
       },
       {
         id: 2,
-        image: 'http://lorempixel.com/400/100/food/food/',
-        description: 'Alter triticums ducunt ad elevatus. A falsis, quadra teres parma.'
-      }
+        image: 'http://lorempixel.com/400/100/technics/',
+        discount: 10,
+        description: 'Alter triticums ducunt ad elevatus. A falsis, quadra teres parma.',
+        storeName: 'Tottus',
+        storeUrl: 'https://www.falabella.cl/',
+        storeImage: 'fallabela.jpg'
+      },
+      {
+        id: 3,
+        image: 'http://lorempixel.com/400/100/transport/',
+        discount: 5,
+        description: 'Alter triticums ducunt ad elevatus. A falsis, quadra teres parma.',
+        storeName: 'Tottus',
+        storeUrl: 'https://www.tottus.cl/tottus/',
+        storeImage: 'lider.jpg'
+      },
+      {
+        id: 4,
+        image: 'http://lorempixel.com/400/100/nightlife/',
+        discount: 5,
+        description: 'Alter triticums ducunt ad elevatus. A falsis, quadra teres parma.',
+        storeName: 'Tottus',
+        storeUrl: 'https://www.sodimac.cl/',
+        storeImage: 'sodimac.png'
+      },
+      {
+        id: 5,
+        image: 'http://lorempixel.com/400/100/nature/',
+        discount: 5,
+        description: 'Alter triticums ducunt ad elevatus. A falsis, quadra teres parma.',
+        storeName: 'Tottus',
+        storeUrl: 'https://www.easy.cl/',
+        storeImage: 'easy.png'
+      },
     )
   }
 
