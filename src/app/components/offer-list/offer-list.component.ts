0a33e744add4e4a7da9bc66110cafa334eeecba5
carlos valdés <valdescarlos17@gmail.com>
2020-05-02 22:57:41 -0400

    offer list component added

---
+++ src/app/components/offer-list/offer-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import Offer from "../../classes/Offer";
+
+@Component({
+  selector: 'app-offer-list',
+  templateUrl: './offer-list.component.html',
+  styleUrls: ['./offer-list.component.css']
+})
+export class OfferListComponent implements OnInit {
+
+  offer_list: Offer[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.offer_list.push(
+      {
+        id: 1,
+        image: 'http://lorempixel.com/400/100/food/food/',
+        description: 'Alter triticums ducunt ad elevatus. A falsis, quadra teres parma.'
+      },
+      {
+        id: 2,
+        image: 'http://lorempixel.com/400/100/food/food/',
+        description: 'Alter triticums ducunt ad elevatus. A falsis, quadra teres parma.'
+      }
+    )
+  }
+
+}
