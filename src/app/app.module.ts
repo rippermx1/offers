3a0333d4c2458b4ff12b2dec0b9bedcfb7e68a04
carlos valdés <valdescarlos17@gmail.com>
2020-05-02 22:58:16 -0400

    new components include in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OfferComponent } from './components/offer/offer.component';
+import { OfferListComponent } from './components/offer-list/offer-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OfferComponent
+    OfferComponent,
+    OfferListComponent
   ],
   imports: [
     BrowserModule,
