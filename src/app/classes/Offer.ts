1eb23312d076948b7c146030e64c8c7185e7ac7a
carlos valdés <valdescarlos17@gmail.com>
2020-05-03 11:49:11 -0400

    offer class attributes added

--- src/app/classes/Offer.ts
+++ src/app/classes/Offer.ts
@@ -2,4 +2,9 @@ export default class Offer {
   id: number;
   image: string;
   description: string;
+  discount?: number;
+  storeName?: string;
+  storeUrl?: string;
+  storeImage?: string;
+  rate?: number;
 }
